955900bcb734833cda3a91cdf8f424046a1ddb92
HyunnoH <hyunnoh01@gmail.com>
2019-05-30 18:14:59 +0900

    second

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(1+4);
+//index.js
+
+var express = require('express');
+var app = express();
+
+app.get('/',function(req, res) {
+	res.send('Hello world!');
+});
+
+app.listen(3000, function(){
+	console.log('Server on');
+});
