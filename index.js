8d38048f1346017bddddf122bea4eb50c2b1ab91
HyunnoH <hyunnoh01@gmail.com>
2019-05-31 22:42:35 +0900

    second heroku

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ app.set("views", "view");
 app.set("view engine","ejs");
 app.use(express.static(__dirname + '/public'));
 
-
-
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
 	console.log('Server on');
