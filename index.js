13e659d3412c659e7d11e07ab0cff130d56ca3c1
HyunnoH <hyunnoh01@gmail.com>
2019-06-13 15:56:06 +0900

    2019-06-13

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //index.js
-
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
@@ -11,8 +10,59 @@ var connection = mysql.createConnection({
   database : 'heroku_91e7a73ec3ad857'
 });
 
+var json = {
+	"target" : '*',
+	"table" : 'customer'
+}
+
 connection.connect();
-connection.end();
+
+function select_query(data) {
+	query = "SELECT";
+	string = query.concat(" ", JSON.parse(data).target, " ", "FROM ", JSON.parse(data).table, ";");
+	console.log(string);
+	console.query(string, function(err, results, fields) {
+		if(err){
+			console.log(err);
+		}
+		console.log(results);
+	});
+}
+
+connection.query('SELECT * FROM customer;', function(err, results, fields) {
+	if(err){
+		console.log(err);
+	}
+	console.log(results);
+});
+
+select_query(json);
+/**
+ * [description]
+ * @param  {[type]} err     [description]
+ * @param  {[type]} results [description]
+ * @param  {[type]} fields) {	if(err){		console.log(err);	}	console.log(results);} [description]
+ * @return {[type]}         [description]
+ * connection.query('INSERT INTO customer VALUES ("Minsu", 20);', function(err, results, fields) {
+	if(err){
+		console.log(err);
+	}
+	console.log(results);
+});
+ */
+connection.query('DELETE FROM customer WHERE CustomerName = "Minsu";', function(err, results, fields) {
+	if(err) {
+		console.log(err);
+	}
+	console.log(results);
+});
+
+connection.query('SELECT * FROM customer;', function(err, results, fields) {
+	if(err){
+		console.log(err);
+	}
+	console.log(results);
+});
 
 app.set("views", "view");
 app.set("view engine","ejs");
@@ -25,3 +75,4 @@ app.listen(port, function(){
 	console.log("Now server are waiting for the client's access...");
 });
 
+connection.end();
