e4ff851b1c088b47e0363848e0708d913aa88235
HyunnoH <hyunnoh01@gmail.com>
2019-06-19 13:38:41 +0900

    /list enter added

--- index.js
+++ index.js
@@ -2,27 +2,34 @@
 
 var express = require('express');
 var mysql = require('mysql');
+var mysql_data = require('./config.js');
 var app = express();
-
+/*
 var connection = mysql.createConnection({
 	host     : 'us-cdbr-iron-east-02.cleardb.net',
 	user     : 'b387ea294ad5d8',
 	password : '6c97eafa',
 	database : 'heroku_91e7a73ec3ad857'
 });
+ */
+function startConnection() {
+	connection = mysql.createConnection(mysql_data);
+	connection.connect(function(err) {
+		if (err) {
+			console.error('CONNECT FAILED', err.code);
+			startConnection();
+		}
+		else
+			console.error('CONNECTED');
+	});
+	connection.on('error', function(err) {
+		if (err.fatal)
+			startConnection();
+	});
+}
 
-connection.connect();
+startConnection();
 
-connection.query("select * from customer" , 
-    	function (error, result, fields) {
-            if (error) { //에러 발생시
-            	res.send('err : ' + error)
-            }
-            else { //실행 성공
-            	res.send('data : ' + result);
-            }
-    });
-    
 app.set("views", "view");
 app.set("view engine","ejs");
 app.use(express.static(__dirname + '/public'));
@@ -33,20 +40,21 @@ app.listen(port, function(){
 	console.log("Express server is open on port 3000.");
 });
 
-app.get('/select', function(req, res) {
-	console.log("Someone entered in '/select'.");
-	connection.query("SELECT * FRO8M customer", function(err, rows, fields) {
-		if(!err)
-			console.log('The solution is ', rows);
-		else
-			console.log('Error, ', err);
-	});
-});
-
 app.get('/list', function (req, res) {
     //SQL문 실행
-	    
+    console.log('Connection to the /list sensored.');
+    connection.query('SELECT * FROM customer', function(err, rows, fields) {
+    	if(err){
+    		console.log('Error occured! ', err);
+    		res.send("There's no data you requested.");
+    	}
+    	else{
+    		console.log('Data is: ', rows);
+    		res.json(rows);
+    	}
+    });
+
 });
 
-
-connection.end();
+connection.on('error', function(error) {console.log(error)});
+//connection.end();
